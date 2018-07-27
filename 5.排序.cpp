05f6f48b09a127e3c766125464b7bb13683618d7
GuoJaw <39326730+GuoJaw@users.noreply.github.com>
2018-07-27 20:43:37 +0800

    Update 5.排序.cpp

--- 5.排序.cpp
+++ 5.排序.cpp
@@ -3,103 +3,86 @@
 #include<iostream>
 using namespace std;
 #define STRLEN(A) (sizeof((A))/sizeof((A[0])))
-//二分查找
-int binarySearch(int A[], int n, int key){
-	int left = 0, right = n - 1;
-	while (left <= right){ // <=
-		int mid = (left + right) / 2;
-		if (A[mid] == key)
-			return mid;
-		else if (A[mid]>key)
-			right = mid - 1;
-		else
-			left = mid + 1;
-	}
-	return -1;
-}
+
 void swap(int& a, int& b){
 	a ^= b; b ^= a; a ^= b;
 }
-//冒泡排序
-void bubbleSort(int A[], int n){
-	for (int i = 0; i < n - 1; i++){ //进行n-1趟排序
-		bool flag = false;//记录该趟是否发生了交换
-		for (int j = 0; j < n - 1 - i; j++){
-			if (A[j] > A[j + 1]){ //前一个数>后一个数，就交换
+void bubble_sort(int A[], int n){
+	for (int i = 0; i < n - 1; i++){//进行n-1趟
+		bool flag = false;//标记该趟是否发生了交换
+		for (int j = 0; j < n - 1 - i; j++){//每趟确定一个最大值末尾(终端)
+			if (A[j]>A[j + 1]){
 				swap(A[j], A[j + 1]);
 				flag = true;
 			}
 		}
-		if (flag == false)//如果没有发生交换，说明已经有序，直接退出
+		if (flag == false)
 			break;
 	}
 }
-//选择排序
-void selectSort(int A[], int n){
-	for (int i = 0; i < n - 1; i++){ //进行n-1趟
-		int min = i;//先将i设为最小值
-		for (int j = i + 1; j < n; j++){//从A[i,n)选出最小值
+void select_sort(int A[], int n){
+	for (int i = 0; i < n - 1; i++){//进行n-1趟
+		int min = i;
+		for (int j = i + 1; j < n; j++){
 			if (A[min]>A[j])
 				min = j;
 		}
-		if (min != i) 
+		if (min != i)//当min不等于初始的i，说明min发生变化-->交换
 			swap(A[min], A[i]);
 	}
 }
-/*直接插入排序
-(1)确定待插入元素位置:[1,n-1]
-(2)将A[i]插入到A[0]~A[i-1]位置
-	[1]tmp保存待插入元素
-	[2]寻找插入位置&&越界判断，后移
-	[3]插入
-*/
-void insertSort(int A[], int n){
+int binary_search(int A[], int n, int key){//二分查找
+	int l = 0, r = n - 1;
+	while (l <= r){
+		int mid = (l + r) / 2;
+		if (A[mid] = key)
+			return mid;
+		else if (A[mid] > key)
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return -1; //没查找到
+}
+void insert_sort(int A[], int n){
 	int j;
-	for (int i = 1; i < n; i++){//将A[i]插入有序序列A[1]，A[2]，...，A[N-1]中
-		if (A[i - 1]>A[i]){ //如果逆序
-			int tmp = A[i];//[1]保存待插元素A[i]
-			for (j = i - 1; j >= 0 && A[j] > tmp; j--)//[2]后移
+	for (int i = 1; i < n; i++){//待插元素[1,n-1]
+		if (A[i] < A[i - 1]){ //将A[i]插入到(有序的[0,i-1]中)
+			int tmp = A[i]; //记录A[i]
+			for (j = i - 1; j >= 0 && A[j]>tmp; j--)//寻找插入位置，并后移
 				A[j + 1] = A[j];
-			A[j+1] = tmp;//[3]插入
+			//查找到插入位置为j+1
+			A[j + 1] = tmp;
 		}
 	}
 }
-//折半插入排序
 void binary_insert_sort(int A[], int n){
-	for (int i = 1; i < n; i++){//待插入元素A[1]~A[n-1]
-		//寻找插入位置：left或right+1
-		int left = 0, right = i - 1;
-		while (left <= right){
-			int mid = (left + right) / 2;
-			if (A[mid] < A[i])
-				left = mid + 1;
+	for (int i = 1; i < n; i++){//待插元素[1,n-1]
+		//查找插入位置:l
+		int l = 0, r = i - 1;
+		while (l <= r){
+			int mid = (l + r) / 2;
+			if (A[mid]<A[i])
+				l = mid + 1;
 			else
-				right = mid - 1;
+				r = mid - 1;
 		}
-		//保存待插入元素
+		//保存带插入元素
 		int tmp = A[i];
-		//后移
-		for (int j = i - 1; j>=left; j--)
+		for (int j = i - 1; j >= l; j--)//后移
 			A[j + 1] = A[j];
-		//插入
-		A[left] = tmp;
+		A[l] = tmp;
 	}
 }
-
-/*
-快速排序：分治策略
-（1）至少有两个数，才进行（quick_sort快速排序，partition划分操作）因此，
-		条件分别是：if( l < r ) ， while( l < r )
-（2）partition划分操作：先从右到左，寻找第一个小于pivot的值A[r] ----> 执行A[l] = A[r]
-（3）partition划分操作的返回值是： A[l] = pivot;     return l ;
-*/
+//快速排序
 int partition(int A[], int l, int r){
-	int pivot = A[l]; //基准
+	int pivot = A[l]; //将第一个元素设为基准
 	while (l < r){
-		while (l < r && pivot <= A[r])//从右到左
+		while (l < r&&pivot <= A[r]) 
 			r--;
 		A[l] = A[r];
-		while (l < r && A[l] <= pivot)//从左到右
+
+		while (l < r&&pivot >= A[l])
 			l++;
 		A[r] = A[l];
 	}
@@ -107,7 +90,7 @@ int partition(int A[], int l, int r){
 	return l;
 }
 void quick_sort(int A[], int l, int r){
-	if (l < r){//至少两个数，才进行快排
+	if (l < r){
 		int pivot = partition(A, l, r);
 		quick_sort(A, l, pivot - 1);
 		quick_sort(A, pivot + 1, r);
@@ -122,7 +105,7 @@ void printA(int A[], int n){
 void test01(){
 	int A[] = { 1, 2, 4, 3, 1, 7, 9, 8 };
 	int len = STRLEN(A);
-	binary_insert_sort(A, len);
+	select_sort(A, len);
 	printA(A, len);
 }
 void test02(){
